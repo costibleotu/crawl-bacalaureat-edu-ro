7b8f74c2c7193fe6a4fe5d3486940cb53bebf206
Costin Bleotu <costi@databus.pro>
2017-10-03 12:03:42 +0300

    Fixed all years

--- bacalaureat_edu_ro/bacalaureat_edu_ro/spiders/bacalaureat.py
+++ bacalaureat_edu_ro/bacalaureat_edu_ro/spiders/bacalaureat.py
@@ -4,22 +4,30 @@ from datetime import datetime
 from bs4 import BeautifulSoup as soup
 import re
 from bacalaureat_edu_ro.items import BacalaureatEduRoItem
-
+import base64
 
 class BacalaureatSpider(scrapy.Spider):
     name = 'bacalaureat'
     allowed_domains = ['bacalaureat.edu.ro']
-    counties = ["AB","AR","AG","BC","BH","BN","BT","BR","BV","B","BZ","CL","CS","CJ","CT","CV","DB","DJ","GL","GR","GJ","HR","HD","IL","IS","IF","MM","MH","MS","NT","OT","PH","SJ","SM","SB","SV","TR","TM","TL","VL","VS","VN"]
     start_urls = []
 
-    def __init__(self, after=None, year=None):
+    def __init__(self, session=None, year=None, ):
         if year:
             self.year = int(year)
         else:
             self.year = datetime.now().year
 
+        if session:
+            self.session = int(session)
+        else:
+            self.session = 1
+
+
     def start_requests(self):
-        url = 'http://static.bacalaureat.edu.ro/{}/rapoarte/rezultate/alfabetic/page_1.html'.format(self.year)
+        if self.session == 1:
+            url = 'http://static.bacalaureat.edu.ro/{}/rapoarte/rezultate/alfabetic/page_1.html'.format(self.year)
+        else:
+            url = 'http://static.bacalaureat.edu.ro/{}/rapoarte_sept/rezultate/alfabetic/page_1.html'.format(self.year)
         yield scrapy.Request(
             url=url,
             callback=self.parse,
@@ -27,9 +35,16 @@ class BacalaureatSpider(scrapy.Spider):
 
     def parse(self, response):
         root = soup(response.body, 'html.parser')
+        if self.year in [2010, 2011, 2012, 2013, 2014, 2015, 2016]:
+            regex = r'^.*ged\(\){return \"(.*)\";}'
+            ged = re.findall(regex, root.find('script').text)[0]
+            dec_ged = base64.b64decode(s3(ged))
+            root = soup(dec_ged, 'html.parser')
+
+
         tabel = root.find('table', {'id': 'mainTable'})
+
         row_1 = True
-        # print(tabel)
         for elev in tabel.find_all('tr')[2:]:
             td_elev = elev.find_all('td')
             if row_1:
@@ -52,7 +67,7 @@ class BacalaureatSpider(scrapy.Spider):
                 item['lb_moderna_oral'] = td_elev[15].text.replace('\xa0', ' ')
                 item['disciplina_obligatorie_scris'] = td_elev[16].text.replace('\xa0', ' ')
                 item['disciplina_alegere_aria_culiculara'] = td_elev[17].text.replace('\xa0', ' ')
-                if self.year not in [2017]:
+                if self.year not in [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]:
                     item['disciplina_alegere_celelalte_arii_culiculare'] = td_elev[18].text.replace('\xa0', ' ')
                     if self.year not in [2009]:
                         item['nume'] = td_elev[1].text.strip().replace('\xa0', ' ')
@@ -88,7 +103,7 @@ class BacalaureatSpider(scrapy.Spider):
                 item['disciplina_alegere_aria_culiculara_nota'] = td_elev[7].text.replace('\xa0', ' ')
                 item['disciplina_alegere_aria_culiculara_contestatie'] = td_elev[8].text.replace('\xa0', ' ')
                 item['disciplina_alegere_aria_culiculara_final'] = td_elev[9].text.replace('\xa0', ' ')
-                if self.year not in [2017]:
+                if self.year not in [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]:
                     item['disciplina_alegere_celelalte_arii_culiculare_nota'] = td_elev[10].text.replace('\xa0', ' ')
                     item['disciplina_alegere_celelalte_arii_culiculare_contestatie'] = td_elev[11].text.replace('\xa0', ' ')
                     item['disciplina_alegere_celelalte_arii_culiculare_final'] = td_elev[12].text.replace('\xa0', ' ')
@@ -105,3 +120,44 @@ class BacalaureatSpider(scrapy.Spider):
                 url=next_url,
                 callback=self.parse,
                 meta={'year': self.year, 'county': item['judet']})
+
+
+def str_replace(srch, rplc, sbjct):
+    if len(sbjct) == 0:
+        return ''
+
+    if len(srch) == 1:
+        return sbjct.replace(srch[0], rplc[0])
+
+    lst = sbjct.split(srch[0])
+    reslst = []
+    for s in lst:
+        reslst.append(str_replace(s, srch[1:], rplc[1:]))
+    return rplc[0].join(reslst)
+
+
+def s0(a, b, c):
+    a = str_replace(b, '_', a)
+    a = str_replace(c, b, a)
+    a = str_replace('_', c, a)
+    return a
+
+
+def s1(a, b):
+    return s0(a, b.lower(), b.upper())
+
+
+def s2(a):
+    for i in ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]:
+        a = s1(a, i)
+    return a
+
+
+def s3(a):
+    a = s0(a, "0", "O")
+    a = s0(a, "1", "l")
+    a = s0(a, "5", "S")
+    a = s0(a, "m", "s")
+    a = s2(a)
+
+    return a
